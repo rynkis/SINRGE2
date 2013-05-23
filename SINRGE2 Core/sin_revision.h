54e52dbf19f2ffe67f454095ee6b4bdc3f7fa3f8
Lynch Sherry <Gernischt@gmail.com>
2013-05-23 18:18:44 +0800

    next

--- SINRGE2 Core/sin_revision.h
+++
@@ -1,7 +0,0 @@
-#ifndef __SIN_REVISION_H__
-#define __SIN_REVISION_H__
-
-#define BUILD_DATA_STR  "2,1,2,126"
-#define BUILD_DATA      2,1,2,126
-
-#endif // __SIN_REVISION_H__
--- demos/DemoIME/__list__.rb
+++
@@ -1,6 +0,0 @@
-#encoding:utf-8
-
-require "common.rb"
-require "hanzi.rb"
-require "pinyin.rb"
-require "SINIME.rb"
